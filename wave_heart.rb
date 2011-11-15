520c5a8923adabe74249fb2c952eeb342f69329a
Mars <mars@WunderBook.local>
2011-11-14 22:08:05 -0800

    Draft implementation of an AudioQueue
    Following
    http://developer.apple.com/library/mac/#documentation/MusicAudio/Concept
    ual/AudioQueueProgrammingGuide/AQPlayback/PlayingAudio.html

---
+++ wave_heart.rb
@@ -0,0 +1,120 @@
+# MacRuby 0.10
+
+framework 'AppKit'
+framework 'AudioToolbox'
+
+class WaveHeart
+  
+  # An AudioQueue pushing an audio stream into CoreAudio HAL.
+  #
+  class Vessel
+    attr_reader :state
+    
+    State = Struct.new('State', 
+      :data_format,               # AudioStreamBasicDescription  
+      :queue,                     # AudioQueueRef                
+      :buffers,                   # AudioQueueBufferRef          
+      :audio_file,                # AudioFileID                  
+      :buffer_byte_size,          # UInt32                       
+      :current_packet,            # SInt64                       
+      :num_packets_to_read,       # UInt32                       
+      :packet_descs,              # AudioStreamPacketDescription 
+      :is_running )               # bool                         
+    
+    def initialize(file_path=nil)
+      @state = State.new
+      load(file_path) if file_path
+    end
+  
+    # Callback from the AudioQueue to refill a playback buffer.
+    #
+    def handle_output_buffer(
+      state,                      # State
+      queue,                      # AudioQueueRef
+      buffer )                    # AudioQueueBufferRef
+    
+      return unless @state.is_running
+      num_of_packets = @state.num_packets_to_read
+  
+      AudioFileReadPackets( 
+        @state.audio_file,
+        false,
+        bytes_read,
+        @state.packet_descs,
+        @state.current_packet,
+        num_of_packets,
+        buffer )
+  
+      if (packets_read > 0)
+        AudioQueueEnqueueBuffer(
+          @state.queue,
+          buffer,
+          @state.packet_descs ? num_of_packets : 0,
+          @state.packet_descs )
+        @state.current_packet += num_of_packets
+      else
+        AudioQueueStop(@state.queue, false)
+        @state.is_running = false
+      end
+    end
+    
+    MaxBufferSize = 0x50000
+    MinBufferSize = 0x4000
+    
+    # Calculate what to read as the audio queue drains.
+    #
+    # Returns array of buffer size & number of packets to read.
+    #
+    def derive_buffer_size(
+      data_format,                    # AudioStreamBasicDescription
+      max_packet_size,                # UInt32                     
+      seconds,                        # Float64                    
+      out_buffer_size=nil,            # UInt32                     
+      out_num_packets_to_read=nil )   # UInt32                     
+      
+      
+      if data_format.mFramesPerPacket != 0
+        num_packets_for_time =
+          data_format.mSampleRate / data_format.mFramesPerPacket * seconds
+        out_buffer_size = num_packets_for_time * max_packet_size
+      else
+        out_buffer_size =
+          MaxBufferSize > max_packet_size ?
+            MaxBufferSize : max_packet_size
+      end
+      
+      if out_buffer_size > MaxBufferSize &&
+        out_buffer_size > max_packet_size
+          out_buffer_size = MaxBufferSize
+      elsif out_buffer_size < MinBufferSize
+        out_buffer_size = MinBufferSize;
+      end
+      
+      out_num_packets_to_read = out_buffer_size / max_packet_size;
+      
+      [out_buffer_size, out_num_packets_to_read]
+    end
+    
+    def load(file_path)
+      audio_file_url = CFURLCreateFromFileSystemRepresentation(
+        NULL, file_path, file_path.size, false )
+      result = AudioFileOpenURL(
+        audio_file_url, KfsRdPerm, 0, @state.audio_file )
+      CFRelease(audio_file_url)
+      
+      data_format_size = @state.data_format.size
+      AudioFileGetProperty(
+        @state.audio_file, KAudioFilePropertyDataFormat, data_format_size, @state.data_format )
+      
+      init_queue
+    end
+    
+    def init_queue
+      return if @state.queue
+      AudioQueueNewOutput(
+        @state.data_format, handle_output_buffer, @state, 
+        CFRunLoopGetCurrent, KCFRunLoopCommonModes, 0,
+        @state.queue )
+    end
+  end
+end
